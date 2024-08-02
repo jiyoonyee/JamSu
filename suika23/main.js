78532771b9d113944afcf78358a7613b61785607
jiyoonyee <a01037651377@gmail.com>
2024-08-02 11:17:23 +0900

    수박생성

--- suika23/main.js
+++ suika23/main.js
@@ -1,83 +1,121 @@
 import { FRUITS } from "./fruits.js";
 
+// 모듈 불러오기
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    World = Matter.World;
+    World = Matter.World,
+    // 조작을 위해 Body 선언
+    Body = Matter.Body;
 
 // 엔진 선언
 const engine = Engine.create();
 
-// 렌더 선언
+// 렌더 선언(배경)
 const render = Render.create({
     engine,
-    element : document.body,
-    options:{
-        wireframes : false,
-        background : '#F7F4C8',
-        width : 620,
-        height : 850,
+    element: document.body,
+    options: {
+        wireframes: false,  // true 배경색이 적용이 안됩니다.
+        background: '#F7F4C8',
+        width: 620,
+        height: 850,
+    }
+});
 
-    },
-})
-
-//벽 배치를 위한 world 선언
+// 벽 배치를 위한 world 선언
 const world = engine.world;
 
-const leftWall = Bodies.rectangle(15,395,30,790,{
+// 왼쪽 벽 생성
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+    isStatic: true, // 고정해 주는 기능
+    render: { fillStyle: '#E6B143'} // 색상 지정
+});
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+    isStatic: true,
+    render: { fillStyle: '#E6B143'}
+});
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+    isStatic: true,
+    render: { fillStyle: '#E6B143'}
+});
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
     isStatic : true,
-    render:{ fillStyle : '#E6B143'},
-})
+    isSensor : true,    // 충돌은 감지하는데, 물리엔진은 적용 안함
+    render: { fillStyle: '#E6B143'}
+});
 
-const rightWall = Bodies.rectangle(605,395,30,790,{
-    isStatic : true,
-    render:{ fillStyle : '#E6B143'},
-})
+// 벽 월드에 배치
+World.add(world, [leftWall, rightWall, ground, topLine]);
 
-const ground = Bodies.rectangle(310,820,620,60,{
-    isStatic : true,
-    render:{ fillStyle : '#E6B143'},
-})
 
-const topLine = Bodies.rectangle(310,150,620,2,{
-    isStatic : true,
-    isSensor : true, // 충돌은 갑지하는 물리엔진은 감지 안함
-    render:{ fillStyle : '#E6B143'},
-})
-
-//벽 월드에 배치
-
-World.add(world,[leftWall, rightWall, ground,topLine])
-
-Render.run(render)
-Runner.run(engine)
+Render.run(render);
+Runner.run(engine);
 
 let currentBody = null;
 let currentFruit = null;
+let disableAction = false;
 
-function addFruit(){
+function addFruit() {
 
     // 과일 인덱스값 저장
-    const index = Math.floor(Math.random()*5);
+    const index = Math.floor(Math.random() * 5);
 
-    //과일 이미지 경로 불러오기 변수
+    // 과일 이미지 경로 불러오기 변수
     const fruit = FRUITS[index];
 
-    const body = Bodies.circle(300,50,fruit.radius,{
+    const body = Bodies.circle(300, 50, fruit.radius, {
         index : index,
-        isSleeping : true, //시작시 바로 떨어지지 않고 잠시 멈춤
+        isSleeping : true,  // 시작시 바로 떨어지지 않고 잠시 멈춤
         render : {
             sprite : { texture : `${fruit.name}.png`},
         },
-        //통통 튀기는 설정
-        restitution : 1,
+
+        // 통통 튀기는 설정
+        restitution : 0.5,
     })
 
     currentBody = body;
     currentFruit = fruit;
 
-    World.add(world,body)
+    World.add(world, body)
 }
 
-addFruit()
+// 키보드 입력 함수
+window.onkeydown = (event) => {
+
+    if(disableAction)
+        return;
+
+    switch(event.code) {
+        case "KeyA":
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x - 10,
+                y: currentBody.position.y,
+            });
+            break;
+        case "KeyD":
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x + 10,
+                y: currentBody.position.y,
+            });
+            break;
+        case "KeyS":
+            // isSleeping을 false로 해서 과일을 떨어트림
+            currentBody.isSleeping = false;
+            // 1초 대기 후 새로운 과일 생성
+            disableAction = true;
+            setTimeout(() => {
+                addFruit();
+                disableAction = false;
+            },1000)
+            break;
+
+    }
+}
+
+addFruit();
